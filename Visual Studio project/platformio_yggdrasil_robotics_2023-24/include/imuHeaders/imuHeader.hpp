248718c40724492a86b629d016ecca1abdac7259
Nico <nicolascereijoranchal@gmail.com>
2023-09-24 15:10:26 +0200

    IMU control refactored

--- Visual Studio project/platformio_yggdrasil_robotics_2023-24/include/imuHeaders/imuHeader.hpp
+++ Visual Studio project/platformio_yggdrasil_robotics_2023-24/include/imuHeaders/imuHeader.hpp
@@ -2,19 +2,14 @@
 
 class MPU6050_getdata {
   private:
-    //int16_t ax, ay, az, gx, gy, gz;
-    int16_t gz;
     //float pith, roll, yaw;
-    unsigned long now, lastTime = 0;
-    //Differential time
-    float dt;
-    //Angle variable
-    float agz = 0;
-    //Gyroscope offset
-    long gzo = 0;
+    unsigned long lastTime = 0;
+    float differentialTime;
+    float expectedAngle = 0;
+    long gyroscopeOffset = 0;
     
   public:
-    bool MPU6050_dveInit(void);
-    bool MPU6050_calibration(void);
-    bool MPU6050_dveGetEulerAngles(float *Yaw);
+    bool MPU6050Init(void);
+    void MPU6050Calibration(void);
+    float MPU6050GetEulerAngles();
 };
--- Visual Studio project/platformio_yggdrasil_robotics_2023-24/src/sources/imuControl/imuControl.cpp
+++ Visual Studio project/platformio_yggdrasil_robotics_2023-24/src/sources/imuControl/imuControl.cpp
@@ -8,63 +8,53 @@
 MPU6050 accelgyro;
 MPU6050_getdata MPU6050Getdata;
 
-// static void MsTimer2_MPU6050getdata(void) {
-//   sei();
-//   int16_t ax, ay, az, gx, gy, gz;
-//   accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz); //读取六轴原始数值
-//   float gyroz = -(gz - MPU6050Getdata.gzo) / 131 * 0.005f;
-//   MPU6050Getdata.yaw += gyroz;
-// }
+bool MPU6050_getdata::MPU6050Init() {
+  uint8_t chipID = 0x00;
+  int attempts = 0;
 
-bool MPU6050_getdata::MPU6050_dveInit(void) {
   Wire.begin();
-  uint8_t chip_id = 0x00;
-  uint8_t cout;
 
+  //Make sure slave device is online
   do {
-    chip_id = accelgyro.getDeviceID();
-    Serial.print("MPU6050_chip_id: ");
-    Serial.println(chip_id);
-    delay(10);
-    cout += 1;
+    chipID = accelgyro.getDeviceID();
+    attempts++;
 
-    if (cout > 10)
-      return true;
-  } while (chip_id == 0X00 || chip_id == 0XFF); //确保从机设备在线（强行等待 获取 ID ）
+    if (attempts > 1000) {
+      Serial.println("Connection error with the imu");
+      return false;
+    }
+
+  } while (chipID == 0X00 || chipID == 0XFF);
+  
+  Serial.println("MPU6050 chip id: " + chipID);
   accelgyro.initialize();
-  // unsigned short times = 100; //采样次数
-  // for (int i = 0; i < times; i++) {
-  //   gz = accelgyro.getRotationZ();
-  //   gzo += gz;
-  // }
-  // gzo /= times; //计算陀螺仪偏移
-  return false;
+
+  return true;
 }
 
-bool MPU6050_getdata::MPU6050_calibration(void) {
-  unsigned short times = 100; //采样次数
+void MPU6050_getdata::MPU6050Calibration() {
+  uint8_t totalSamples = 100;
   
-  for (int i = 0; i < times; i++) {
-    gz = accelgyro.getRotationZ();
-    gzo += gz;
+  for (int i = 0; i < totalSamples; i++) {
+    gyroscopeOffset += accelgyro.getRotationZ();
   }
-  gzo /= times; //计算陀螺仪偏移
 
-  // gzo = accelgyro.getRotationZ();
-  return false;
+  //Calculate gyroscope displacement
+  gyroscopeOffset /= totalSamples;
 }
 
-bool MPU6050_getdata::MPU6050_dveGetEulerAngles(float *Yaw) {
-  unsigned long now = millis();   //当前时间(ms)
-  dt = (now - lastTime) / 1000.0; //微分时间(s)
-  lastTime = now;                 //上一次采样时间(ms)
-  gz = accelgyro.getRotationZ();
-  float gyroz = -(gz - gzo) / 131.0 * dt; //z轴角速度
+float MPU6050_getdata::MPU6050GetEulerAngles() {
+  unsigned long actualTime = millis();
+  float actualAngle = 0;
+
+  differentialTime = (actualTime - lastTime) / 1000.0;
+  lastTime = actualTime;
+  // Z axis angular velocity
+  actualAngle = -(accelgyro.getRotationZ() - gyroscopeOffset) / 131.0 * differentialTime;
   
-  if (fabs(gyroz) < 0.05)
-    gyroz = 0.00;
-    
-  agz += gyroz; //z轴角速度积分
-  *Yaw = agz;
-  return false;
+  //Delete noise
+  if (fabs(actualAngle) < 0.05)
+    actualAngle = 0.00;
+  
+  return actualAngle;
 }
